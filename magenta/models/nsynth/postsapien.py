505d31399d37382d867fe3436adc44f1e4bdff1b
aranarunakiri <aran.arunakiri@gmail.com>
2018-08-25 13:14:57 +0200

    t

--- magenta/models/nsynth/postsapien.py
+++ magenta/models/nsynth/postsapien.py
@@ -11,13 +11,13 @@ def start():
         for filex in files:
             file_path = subdir + os.sep + filex
             print 'processing ' + file_path
-            encode(file_path)
+            encode(file_path, filex)
 
 
-def encode(fname):
+def encode(path, filename):
     print('encoding..')
     sr = 16000
-    audio = utils.load_audio(fname, sample_length=40000, sr=sr)
+    audio = utils.load_audio(path, sample_length=40000, sr=sr)
     sample_length = audio.shape[0]
     print('{} samples, {} seconds'.format(sample_length, sample_length / float(sr)))
 
@@ -26,13 +26,13 @@ def encode(fname):
     print(encoding.shape)
     print('finished encoding..')
     # np.save(fname + '.npy', encoding)
-    decode(encoding, fname, sample_length, model_path)
+    decode(encoding, path, filename, sample_length, model_path)
 
 
-def decode(encoding, fname, sample_length, model_path):
+def decode(encoding, path, filename, sample_length, model_path):
     print('decoding..')
     outdir = '/home/paperspace/data/sounds_gen/'
-    fastgen.synthesize(encoding, save_paths=[outdir + 'gen_' + fname], checkpoint_path=model_path,
+    fastgen.synthesize(encoding, save_paths=[outdir + filename], checkpoint_path=model_path,
                        samples_per_save=sample_length)
     print('finished decoding..')
 
