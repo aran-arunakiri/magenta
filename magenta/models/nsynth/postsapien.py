6e535a5b3dcb8f747e1e9ee87418e1857b3f6f93
aranarunakiri <aran.arunakiri@gmail.com>
2018-08-25 13:01:47 +0200

    t

--- magenta/models/nsynth/postsapien.py
+++ magenta/models/nsynth/postsapien.py
@@ -1,24 +1,36 @@
 import numpy as np
 from magenta.models.nsynth import utils
 from magenta.models.nsynth.wavenet import fastgen
+import os
 
-file1 = '395058__mustardplug__breakbeat-hiphop-a4-4bar-96bpm.wav'
-file2 = 'kick1.wav'
-fname = file2
+rootdir = '/home/paperspace/data/sounds'
 
-print('encoding..')
-sr = 16000
-audio = utils.load_audio(fname, sample_length=40000, sr=sr)
-sample_length = audio.shape[0]
-print('{} samples, {} seconds'.format(sample_length, sample_length / float(sr)))
 
-model_path = '/home/paperspace/data/wavenet-ckpt/model.ckpt-200000'
-encoding = fastgen.encode(audio, model_path, sample_length)
-print(encoding.shape)
-print('finished encoding..')
+def start():
+    for subdir, dirs, files in os.walk(rootdir):
+        for filex in files:
+            file_path = subdir + os.sep + filex
+            encode(file_path)
 
-# np.save(fname + '.npy', encoding)
-print('decoding..')
 
-fastgen.synthesize(encoding, save_paths=['gen_' + fname], checkpoint_path=model_path, samples_per_save=sample_length)
-print('finished decoding..')
+def encode(fname):
+    print('encoding..')
+    sr = 16000
+    audio = utils.load_audio(fname, sample_length=40000, sr=sr)
+    sample_length = audio.shape[0]
+    print('{} samples, {} seconds'.format(sample_length, sample_length / float(sr)))
+
+    model_path = '/home/paperspace/data/wavenet-ckpt/model.ckpt-200000'
+    encoding = fastgen.encode(audio, model_path, sample_length)
+    print(encoding.shape)
+    print('finished encoding..')
+    # np.save(fname + '.npy', encoding)
+    decode(encoding, fname, sample_length, model_path)
+
+
+def decode(encoding, fname, sample_length, model_path):
+    print('decoding..')
+    outdir = '/home/paperspace/data/sounds_gen/'
+    fastgen.synthesize(encoding, save_paths=[outdir + 'gen_' + fname], checkpoint_path=model_path,
+                       samples_per_save=sample_length)
+    print('finished decoding..')
