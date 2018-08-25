7ac449cc3531116cea672d3902877a2b0f9bedc7
aranarunakiri <aran.arunakiri@gmail.com>
2018-08-25 09:24:43 +0200

    t

--- magenta/models/nsynth/postsapien.py
+++ magenta/models/nsynth/postsapien.py
@@ -2,13 +2,13 @@ import numpy as np
 from magenta.models.nsynth import utils
 from magenta.models.nsynth.wavenet import fastgen
 
-
 fname = '395058__mustardplug__breakbeat-hiphop-a4-4bar-96bpm.wav'
 sr = 16000
 audio = utils.load_audio(fname, sample_length=40000, sr=sr)
 sample_length = audio.shape[0]
 print('{} samples, {} seconds'.format(sample_length, sample_length / float(sr)))
 
-encoding = fastgen.encode(audio, 'model.ckpt-200000', sample_length)
+model_path = '/home/paperspace/data/wavenet-ckpt/model.ckpt-200000'
+encoding = fastgen.encode(audio, model_path, sample_length)
 print(encoding.shape)
 np.save(fname + '.npy', encoding)
